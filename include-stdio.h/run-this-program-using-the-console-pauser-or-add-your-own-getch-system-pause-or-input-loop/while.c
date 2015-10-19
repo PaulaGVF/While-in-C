c36c428f4612a1729ac3d1130b977146c00cc635
Paula Galindo <paulagalindovf@live.com>
2015-10-19 21:55:37 -0200

    Create while.c

---
+++ include-stdio.h/run-this-program-using-the-console-pauser-or-add-your-own-getch-system-pause-or-input-loop/while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+int main() {
+	
+printf("\n Aqui aparecerao 50 nomeros impares ");
+int num;
+num=1;
+while(num<=150) {
+	
+printf("%d-",num);
+
+num= num+2;
+}
+
+	
+	
+	return 0;
+}
